4c670b49a234dd1abb8fb7c8d8236d061a871e0e
rick <rick.nguyen@tcs.com>
2019-10-01 16:47:23 -0400

    Update ProfileRESTController.java

--- src/main/java/com/application/restControllers/ProfileRESTController.java
+++ src/main/java/com/application/restControllers/ProfileRESTController.java
@@ -1,18 +1,23 @@
 package com.application.restControllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.application.beans.Profile;
+import com.application.repositories.ProfileRepository;
 import com.application.services.ProfileService;
 
 @RestController
 public class ProfileRESTController {
-
+	
 	@Autowired
 	ProfileService profileService;
+	ProfileRepository repo;
 	
 	@PostMapping("/profile")
 	public Profile addProfile(@RequestBody Profile profile) {
@@ -20,4 +25,9 @@ public class ProfileRESTController {
 		return profile;
 	}
 	
+	@RequestMapping("/profile/{id}")
+	@ResponseBody
+	public String getProfile(@PathVariable("id") int id) {
+		return repo.findById(id).toString();
+	}
 }
