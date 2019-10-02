25ad375e8fa866891431b7c30ca0495ec7329230
Michael Rotuno <rotunomp@miamioh.edu>
2019-10-02 15:36:41 -0400

    Add evaluation done

--- src/main/java/com/application/beans/Evaluation.java
+++ src/main/java/com/application/beans/Evaluation.java
@@ -73,12 +73,17 @@ public class Evaluation {
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
-//	public Profile getProfile() {
-//		return profile;
-//	}
-//	public void setProfile(Profile profile) {
-//		this.profile = profile;
-//	}
+	public Profile getProfile() {
+		return profile;
+	}
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+	@Override
+	public String toString() {
+		return "Evaluation [id=" + id + ", reviewerEmail=" + reviewerEmail + ", interviewDate=" + interviewDate
+				+ ", feedback=" + feedback + ", rating=" + rating + ", profile=" + profile + "]";
+	}
 	
 	
 	
